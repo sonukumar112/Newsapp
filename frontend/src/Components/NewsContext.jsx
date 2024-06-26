64be1c96eb4c5ba89b4b6bde406aa2d18e036d0e
manirajyadav-1 <yadavmani41@gmail.com>
2024-06-26 08:59:25 +0545

    Added headers

--- frontend/src/Components/NewsContext.jsx
+++ frontend/src/Components/NewsContext.jsx
@@ -28,6 +28,9 @@ export const NewsProvider = ({ children }) => {
                         country: country,
                         apiKey: import.meta.env.VITE_KEY,
                     },
+                    headers: {
+                      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                    },
                 }
             );
             setArticles(
